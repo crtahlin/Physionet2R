c91034e4d59dd2415337cf091f955aa9cecb0f7c
Crt Ahlin <crt.ahlin@gmail.com>
2017-09-06 06:57:34 +0200

    minor changes

--- code/Import_data_script.R
+++ code/Import_data_script.R
@@ -6,3 +6,7 @@ workingDir <- "../sklop3.physionet.data/sddb/"
 sddb_data <- Import_physionet_HR(workingDir = workingDir)
 
 str(sddb_data)
+
+active_data <- sddb_data
+save(active_data, file = "./physionet_viewer/data/sddb_data.Rdata")
+names(active_data)
