54f958752ecb175b18200de34a03397166491231
Crt Ahlin <crt.ahlin@gmail.com>
2017-11-27 07:14:14 +0100

    edit comments

--- R/cut_data_by_interval_type2.R
+++ R/cut_data_by_interval_type2.R
@@ -8,26 +8,11 @@ cut_data_by_interval_type2 <- function(annotation_data, heart_beat_data) {
   labeled_intervals <- label_interval_type_AFIB(interval_boundaries)
   # calculate interval length
   labeled_intervals[, "Interval_length"] <- (labeled_intervals$Interval_end - labeled_intervals$Interval_start)
-  # add annotation data to heartbeat data
-  # the best way to do this?
-  # maybe do a for loop across all the data and ad to each line the data from the labeled intervals
-  # heart_beat_data[, names(labeled_intervals)] <- NA
-  # for (row in 1:dim(heart_beat_data)[1]) {
-  #   # if time at last value for 
-  #   # print(row)
-  #   if ( heart_beat_data[row, "time"] < tail(labeled_intervals$Interval_end, 1) ) {
-  #     interval_true <- ((heart_beat_data[row, "time"] >= labeled_intervals$Interval_start) & (heart_beat_data[row, "time"] < labeled_intervals$Interval_end))  
-  #   } else {
-  #     interval_true <- (heart_beat_data[row, "time"] == labeled_intervals$Interval_end)
-  #   }
-  #   
-  #   heart_beat_data[row, names(labeled_intervals)] <- labeled_intervals[interval_true,]
-  # }
-  
+
   # add an ID column to labeled_intervals
   labeled_intervals$interval_ID <- 1:dim(labeled_intervals)[1]
   
-  # do an lapply version
+  # define an lapply version
   get_interval_data <- function (x, labeled_intervals) {
     if (x < max(labeled_intervals$Interval_end)) {
       result <- labeled_intervals[((x >= labeled_intervals$Interval_start) & (x < labeled_intervals$Interval_end)) , "interval_ID"] 
@@ -37,6 +22,7 @@ cut_data_by_interval_type2 <- function(annotation_data, heart_beat_data) {
     return(result)
   }
   
+  # run the lapply version
   interval_IDs <- lapply(heart_beat_data$time, get_interval_data, labeled_intervals = labeled_intervals)
   heart_beat_data[, "interval_ID"] <- unlist(interval_IDs)
   result <- merge(heart_beat_data, labeled_intervals, by.x = "interval_ID", by.y = "interval_ID")
