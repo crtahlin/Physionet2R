d7c9732a0394770ef8b27021657d1e074f90227e
Crt Ahlin <crt.ahlin@gmail.com>
2017-11-27 07:15:02 +0100

    edits, bugfixes

--- code/cut_timeseries_before_event.R
+++ code/cut_timeseries_before_event.R
@@ -31,32 +31,18 @@ to_compare <- c("time", "BPM", "Interval_start", "Interval_end", "Interval_type"
 identical(temp[, to_compare], temp2[, to_compare])
 # JUPI!
 
-
-# TODO: tole še v separate funkcijo.
-extract_intervals_for_all_records_in_database <- function (database) {
-  rm("results")
-  for (record in names(database)) {
-    print(record)
-    temp <- cut_data_by_interval_type2(database[[record]]$annotations, database[[record]]$HR_constant_interval)
-    if (exists("results")) {
-      print("previous results exist")
-      results <- rbind(results, temp)
-    } else {
-      print("previous results do not exist")
-      results <- temp}
-  }
-  # add name of database
-  results[, "database"] <- database
-  # return results
-  return(results)
-}
-
-results <- extract_intervals_for_all_records_in_database(sddb_data)
+detach("package:physionet2R", unload = TRUE)
+library(physionet2R)
+str(sddb_data[1])
+results <- extract_intervals_for_all_records_in_database(sddb_data[1:2], db_name = "sddb")
 dim(results)
+str(results)
+unique(results$database)
 
 databases_list <- c("afdb_data", "ltafdb_data", "mitdb_data", "nsr2db_data", "nsrdb_data", "sddb_data")
+rm("all_db_data")
 for (database in databases_list) {
-  results <- extract_intervals_for_all_records_in_database(database = database)
+  results <- extract_intervals_for_all_records_in_database(database = database, db_name = database)
   if (exists("all_db_data")) {
     all_db_data <- rbind(all_db_data, results)
   } else {
