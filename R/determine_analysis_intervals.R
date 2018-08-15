6fae5d199af8dc5bb46b0009e8588c578fc5e146
Crt Ahlin <crt.ahlin@gmail.com>
2018-08-15 11:00:07 +0200

    backup

--- R/determine_analysis_intervals.R
+++ R/determine_analysis_intervals.R
@@ -6,13 +6,13 @@
 determine_analysis_intervals <- function (
   all_db_data_info, # the info about the data - specific to physionet and output of some other function
   Interval_type, # which interval types to keep in analysis
-  Min_interval_length, # how long they have to be at minimum
+  # Min_interval_length , # how long they have to be at minimum
   Keep_total_interval_length = NULL, # shorten interval kept?
   Cut_off_last_X_seconds # cut off some data at interval en?
 ) {
   result <- 
     all_db_data_info[(all_db_data_info$Interval_type == Interval_type & 
-                        all_db_data_info$Interval_length > Min_interval_length), ]
+                        all_db_data_info$Interval_length >= (Keep_total_interval_length + Cut_off_last_X_seconds) ), ]
   # scenario if Keep_total_interval_lenght is set
   # keep only intervals that are still long enough
   # and the start of the interval is moved
@@ -24,7 +24,7 @@ determine_analysis_intervals <- function (
     result$Analysis_interval_start <- result$Interval_start
   }
   
-  result$Min_interval_length <- Min_interval_length
+  # result$Min_interval_length <- Min_interval_length
   result$Cut_off_last_X_seconds <- Cut_off_last_X_seconds
   result$Analysis_interval_end <- result$Interval_end - Cut_off_last_X_seconds
   result$Analysis_interval_length <- result$Analysis_interval_end - result$Analysis_interval_start
